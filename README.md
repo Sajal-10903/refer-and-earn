fc5e262c59ba36a87faf73ecffc198b2d14c9bd8
Sajal-10903 <146699472+Sajal-10903@users.noreply.github.com>
2024-07-11 15:45:37 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Refer & Earn
+
+This is a simple Refer & Earn landing page built with React and Material-UI.
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js
+- npm
+
+### Installation
+
+1. Clone the repository:
+   ```sh
+   git clone https://github.com/your-username/refer-and-earn.git
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-syntax-jsx']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    fallback: {
+      "crypto": false
+    }
+  },
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    port: 8081
+  }
+};
